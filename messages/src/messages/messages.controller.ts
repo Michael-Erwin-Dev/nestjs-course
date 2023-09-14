b935829725d5469bf7530b5b89259b67626ef569
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-14 16:14:50 +0200

    13 adding routing logic

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('/messages')
+export class MessagesController {
+  @Get('/')
+  listMessages() {
+    return [
+      {
+        id: 1,
+        text: 'First message',
+      },
+      {
+        id: 2,
+        text: 'Second message',
+      },
+    ];
+  }
+
+  @Get('/:id')
+  getMessage() {
+    return {
+      id: 1,
+      text: 'First message',
+    };
+  }
+
+  @Post('/')
+  createMessage() {
+    return {
+      id: 1,
+      text: 'First message',
+    };
+  }
+
+  @Put('/:id')
+  updateMessage() {
+    return {
+      id: 1,
+      text: 'First message',
+    };
+  }
+
+  @Delete('/:id')
+  deleteMessage() {
+    return { id: 1, text: 'First message' };
+  }
+}
