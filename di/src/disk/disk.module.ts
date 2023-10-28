9432beaa354e7e83caa85b27169e13d1f64bf99e
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-10-28 13:28:50 +0200

    34. SettingDIBetweenModules 35.DI Between Modules

--- di/src/disk/disk.module.ts
+++ di/src/disk/disk.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
-
+import { PowerModule } from '../power/power.module';
 @Module({
-  providers: [DiskService]
+  imports: [PowerModule],
+  providers: [DiskService],
 })
 export class DiskModule {}
