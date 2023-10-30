86557fa195f8580bdcd1f4bd8d77d4daf53da2a3
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-10-30 17:13:54 +0100

    61. A Few Notes on Exceptions

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -21,8 +22,12 @@ export class UsersController {
     return this.usersService.create(email, password);
   }
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
   @Get()
   findAllUsers(@Query('email') email: string) {
